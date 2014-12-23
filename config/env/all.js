2093c3a8ac5859d87af69197baaca3065e4e24ae
Samruilio <samruilio@gmail.com>
2014-12-24 10:07:25 +1100

    add delete route

--- config/env/all.js
+++ config/env/all.js
@@ -5,7 +5,7 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
     root: rootPath,
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     db: process.env.MONGOHQ_URL,
 
     // Template Engine
