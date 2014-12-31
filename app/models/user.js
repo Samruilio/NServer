0bc22bdfe2f9bd9d5abb85ecdb1a0b4a36c5368a
Samruilio <samruilio@gmail.com>
2014-12-31 16:50:24 +1100

    Added routes

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,10 @@ var UserSchema   = new Schema({
 	name: {type: String, required: true}, 
 	uid: {type: Number, required: true, unique: true},
 	udi: {type: String, required: true, unique: true},
-	secret: {type: String, required: true, unique: true}
+	secret: {type: String, required: true, unique: true}, 
+	status: {type: String, required: true, unique: false, default: 'idle'}, 
+	opponent: {type: String, unique: false, default: ''}, 
+	score: {type: Number, unique: false, default: 1000}
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- config/routes.js
+++ config/routes.js
@@ -17,6 +17,21 @@ module.exports = function(app) {
     	});
     });
 
+    app.get('/api/users/searching/:udi', function(req, res){
+        User.findOneAndUpdate({udi: req.params.udi, opponent: ''}, {status: 'searching'}, {new: true}, function(err, doc) {
+            if (err)
+                res.send(err);
+            else {
+                User.where('status').equals('searching').where('udi').ne(req.params.udi).exec(function(err, doc) {
+                    if (err)
+                        res.send(err);
+                    else
+                        res.json(doc);
+                });
+            }
+        });
+    });
+
     app.get('/api/users/:uid', function(req, res){
     	User.find({uid: req.params.uid}, function(err, users) {
     		if (err)
@@ -26,13 +41,23 @@ module.exports = function(app) {
     	});
     });
 
+    app.get('/api/users/checkready/:udi', function(req, res) {
+        User.find({udi: req.params.udi}, function(err, user) {
+            if (err)
+                res.send(err);
+            else
+                res.json(user);
+        });
+    });
+
     app.post('/api/users/create', function(req, res){
     	var user = new User();
     	User.count({}, function(err, count){
     		user.name = req.body.name;
     		user.uid = count;
     		user.udi = req.body.udi;
-    		user.secret = Math.floor((Math.random() * 10000000) + 1); 
+    		user.secret = Math.floor((Math.random() * 10000000) + 1);
+            user.status = 'idle';
 
     		user.save(function(err) {
     			if (err)
@@ -49,6 +74,52 @@ module.exports = function(app) {
                 res.send(err);
             else
                 res.json(users);
+        });
+    });
+
+    app.post('/api/users/delete/:uid', function(req, res){
+        User.findOneAndRemove({uid: req.params.uid}, function(err, doc) {
+            if (err)
+                res.send(err);
+            else
+                res.json(doc);
+        });
+    });
+
+    app.post('/api/users/set_opponent', function(req, res) {
+        User.findOneAndUpdate({uid: req.body.sel_uid}, {opponent: req.body.opp_uid, status: 'ready'}, {new: true}, function(err, doc) {
+            if (err)
+                res.send(err);
+            else {
+                User.findOneAndUpdate({uid: req.body.opp_uid}, {opponent: req.body.sel_uid, status: 'ready'}, {new: true}, function(err, doc) {
+                    if (err)
+                        res.send(err);
+                    else {
+                        res.json({message: 'wait for accepting'});
+                    }
+                });
+            }
+        });
+    });
+
+    app.post('/api/users/score', function(req, res) {
+        User.find({udi: req.body.udi}, function(err, users) {
+            if (err)
+                res.send(err);
+            else {
+                if(req.body.result == 'win'){
+                    users[0].score += 100;
+                }else if(req.body.result == 'lose'){
+                    users[0].score -= 100;
+                }
+                User.findOneAndUpdate({udi: req.body.udi}, {score: users[0].score, opponent: '', status: 'idle'}, {new: true}, function(err, doc) {
+                    if (err)
+                        res.send(err);
+                    else {
+                        res.json({message: 'succeed'});
+                    }
+                });
+            }
         });
     });
 };
